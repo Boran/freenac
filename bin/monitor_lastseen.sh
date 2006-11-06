cc763c4b6ebf5543aaa731bf736c621116e1afdf
Sean Boran <sean@boran.com>
2006-11-06 06:22:29 +0000

    monitor_lastseen.sh: Patch corrected from vmps to nac.

--- bin/monitor_lastseen.sh
+++ bin/monitor_lastseen.sh
@@ -17,13 +17,13 @@ tempfile2=/tmp/monitor_allows.$$
 
 messagecount=1;
 
-/opt/vmps/logtail /var/log/messages /var/log/.messages.vmps_lastseen | egrep "vmps_lastseen" | wc -l | awk '{if ($1 < limit) print "Warning, is vmps_lastseen working? Only " $1 " messages below threshold " limit ", since last check." }' limit=$messagecount > $tempfile2 2>&1
+/opt/nac/bin/logtail /var/log/messages /var/log/.messages.vmps_lastseen | egrep "vmps_lastseen" | wc -l | awk '{if ($1 < limit) print "Warning, is vmps_lastseen working? Only " $1 " messages below threshold " limit ", since last check." }' limit=$messagecount > $tempfile2 2>&1
 
 # Alert by email and senting to log in DB
 if [ -s $tempfile2 ] ; then
 
   # Log events to vmpslog table, so GUI can see it.
-  #cat $tempfile2| /opt/vmps/bin/vmps_log
+  #cat $tempfile2| /opt/nac/bin/vmps_log
 
   mailx -s "`uname -n` $subject" root         < $tempfile2
   #mailx -s "`uname -n` $subject" sean@boran.com < $tempfile2
