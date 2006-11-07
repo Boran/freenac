63b1005fd43cc1ae9b6e07bf639830ba25ab7f57
Thomas Dagonnier <thomas.dagonnier@gmail.com>
2006-11-07 11:28:07 +0000

    Closing ticket #99 - script doesn't output errors anymore.

--- contrib/import_snmp.php
+++ contrib/import_snmp.php
@@ -29,8 +29,10 @@
 $debug_flag1=false;
 $debug_flag2=false;
 
-include_once "../bin/funcs.inc";               # Load settings & common functions
-$debug_to_syslog=TRUE;
+require_once "../bin/funcs.inc";               # Load settings & common functions
+require_once "../etc/config.inc";  
+$debug_to_syslog=FALSE;
+$logit_to_stdout = FALSE;
 
 define_syslog_variables();              # not used yet, but anyway..
 openlog("import_snmp", LOG_PID, LOG_LOCAL5);
@@ -39,8 +41,9 @@ snmp_set_oid_numeric_print(TRUE);
 snmp_set_quick_print(TRUE);
 snmp_set_enum_print(TRUE); 
 
-// communities --- SHOULD BE MOVED TO CONFIG.INC OR DATABASE
-$snmp_ro = 'SNMPRO';
+// following should be arguments / config / database;
+$snmp_ro = 'public';
+$set_default_vlan = TRUE;
 
 // allow performance measurements
    $mtime = microtime();
@@ -52,10 +55,24 @@ db_connect();
 global $connect, $core_routers, $router_mac_ip_ignore_ip, $router_mac_ip_ignore_mac, $snmpwalk;
 global $router_msc_ip_update_from_dns;
 
-$query_name = '1.3.6.1.2.1.31.1.1.1.1';
-$query_vlan = '1.3.6.1.4.1.9.9.68.1.2.2.1.2';
-$query_trunk = '1.3.6.1.4.1.9.9.46.1.6.1.1.13';
-$query_phys = '1.3.6.1.2.1.31.1.1.1.17';
+
+$snmp_sw['descr'] = 		'SNMPv2-MIB::sysDescr.0';
+$snmp_sw['name'] = 			'SNMPv2-MIB::sysName.0';
+$snmp_sw['location'] = 		'SNMPv2-MIB::sysLocation.0';
+$snmp_sw['contact'] = 		'SNMPv2-MIB::sysContact.0';
+
+$snmp_ifaces = 				'1.3.6.1.2.1.31.1.1.1.1';
+$snmp_cisco['hw'] = 		'SNMPv2-SMI::mib-2.47.1.1.1.1.13';
+
+$snmp_if['name'] = 			'1.3.6.1.2.1.31.1.1.1.1'; 
+$snmp_if['highspeed'] = 	'1.3.6.1.2.1.31.1.1.1.15';				//	10 - 100 - 1000 
+$snmp_if['description'] = 	'1.3.6.1.2.1.31.1.1.1.18';
+$snmp_if['phys'] = 			'1.3.6.1.2.1.31.1.1.1.17';				// true - false
+$snmp_if['trunk'] = 		'1.3.6.1.4.1.9.9.46.1.6.1.1.13';		//	1 : on	2 : off	3 : desirable	4 : auto	5 : onNoNegotiate
+$snmp_if['vlan'] = 			'1.3.6.1.4.1.9.9.68.1.2.2.1.2';
+
+// http://tools.cisco.com/Support/SNMP/do/BrowseOID.do for details
+
 /* 
 	Some SNMP requests
 
@@ -72,50 +89,7 @@ IF-MIB::ifConnectorPresent.10116 = INTEGER: true(1)
 SNMPv2-SMI::enterprises.9.9.46.1.6.1.1.13.10116 = INTEGER: 2
 
 */
-function get_phys($switch,$port_index) {
-        global $snmpwalk;
-	global $snmp_ro;
-        global $query_phys;
 
-        $query = $query_phys.'.'.$port_index;
-#echo "snmpget($switch,$snmp_ro,$query) \n";
-        $value = snmpget($switch,$snmp_ro,$query);
-        if ($value == 1) {
-	  return(TRUE);
-        } else {
-          return(FALSE);
-        };
-};
-function get_trunk($switch,$port_index) {
-        global $snmpwalk;
-	global $snmp_ro;
-        global $query_trunk;
-
-        $query = $query_trunk.'.'.$port_index;
-#echo "snmpget($switch,$snmp_ro,$query) \n";
-        $value = snmpget($switch,$snmp_ro,$query);
-	if ($value == 1) {
-		return(TRUE);
-	} else {
-		return(FALSE);
-	};
-
-};
-
-function get_vlan($switch,$port_index) {
-	global $snmpwalk;
-	global $query_vlan;
-	global $snmp_ro;
-
-	$query = $query_vlan.'.'.$port_index;
-#echo "snmpget($switch,$snmp_ro,$query) \n";
-	$vlan_id = snmpget($switch,$snmp_ro,$query);
-	if ($vlan_id && $vlan_id > 0) {
-          return($vlan_id);
-	} else {
-	  return(FALSE);
-	};
-};
 
 $switches = mysql_fetch_all("SELECT * FROM switch;");
 
@@ -124,38 +98,52 @@ foreach ($switches as $switchrow) {
 	$switchname = $switchrow['name'];
 	$location = $switchrow['location'];
 
-// first, get all interfaces
+//	debug2("snmpwalk $switch for interfaces");
 
-	debug2("snmpwalk $switch for interfaces");
-	//$ifaces=explode("\n", syscall("$snmpwalk $switch $query_name"));
-	$ifaces = snmprealwalk($switch,'public',"1.3.6.1.2.1.31.1.1.1.1");
+	$ifaces = snmprealwalk($switch,$snmp_ro,"1.3.6.1.2.1.31.1.1.1.1");
 
 	foreach ($ifaces as $oid => $name) {
+			$oids = explode('.',$oid);
+			$idx = $oids[12];
+			$iface[$id]['id'] = $idx;
+			$index[] = $idx;
+	};
+	unset($idx);
 
-		$oids = explode('.',$oid);
-		$idx = $oids[12];
-		$vlan = 0;
+	foreach ($snmp_if as $field => $snmp_query) {
+		foreach($index as $idx) {
+			$iface[$idx][$field] = '';
+		};
+		$walk = snmprealwalk($switch,$snmp_ro,$snmp_query);
+		foreach ($walk as $oid => $value) {	
+			$oids = explode('.',$oid);
+			$idx = $oids[count($oids)-1];
+			$iface[$idx][$field] = $value;
+		};
+		unset($walk);
+	};
 
-
-// next is to quickly filter out all non ethernet interfaces
-		if (stristr($name,'Gi') || stristr($name,'Fa')) {
-// the following line gives warning on 3500XL TODO FIX
-			if (get_trunk($switch,$idx)) {
+	foreach ($iface as $idx => $myiface) {
+	// first, check if we have a real interface
+		if (($myiface['phys'] == 1) && ($myiface['trunk'] != 1) &&
+				((stristr($myiface['name'],'Fa') || stristr($myiface['name'],'Gi')))) {
+		// then, if we have a real default vlan & we were called with "set_default_vlan"
+			if ($set_default_vlan === TRUE) {
+				if ($myiface['vlan'] > 1) {
+						$vlan = $myiface['vlan'];
+				} else {
+						$vlan = $default_vlan;
+				};
+				$query .= "INSERT INTO port(switch,name,default_vlan,location) VALUES ('$switch','".$myiface['name']."','$vlan','$location');\n";	
 			} else {
-//				if (get_phys($switch,$idx)) {
-					$vlan = get_vlan($switch,$idx);
-					if ($vlan > 0) {
-						$query .= "INSERT INTO port(switch,name,default_vlan,location) VALUES ('$switch','$name','$vlan','$location');\n";
-					};
-//				};
+				$query .= "INSERT INTO port(switch,name,location) VALUES ('$switch','".$myiface['name']."','$location');\n";	
 			};
+		mysql_query($query) or die ("Unable to insert ports into table\n");
+		unset($query);		
 		};
 	};
-	// mysql_query($query) or die("Unable to insert into table");
-	echo $query;
 };
 
-exit();
 
 
   // measure performance
@@ -167,5 +155,6 @@ exit();
    debug1("Time taken= ".$totaltime." seconds\n");
    #logit("Time taken= ".$totaltime." seconds\n");
 
+exit(0);
 
 ?>
