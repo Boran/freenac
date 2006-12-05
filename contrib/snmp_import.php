3cb6c5ef00543a19a087d7bb0b50dc6a414f401a
Thomas Dagonnier <thomas.dagonnier@gmail.com>
2006-12-05 07:13:56 +0000

    - cf comments from sean : check outputs - added CLI - added UPDATE instead of INSERT if ports already exists

--- contrib/snmp_import.php
+++ contrib/snmp_import.php
@@ -50,31 +50,53 @@ snmp_set_enum_print(TRUE);
 
 db_connect();
 
-$switches =  mysql_fetch_all("SELECT * FROM switch");
 
-foreach ($switches as $switchrow) {
-	$switch = $switchrow['ip'];
-	$switchname = $switchrow['name'];
-	$location = $switchrow['location'];
-        debug2("snmpwalk $switch for interfaces");
 
-	$ifaces = walk_ports($switch,$snmp_ro);
-
-	foreach ($ifaces as $idx => $myiface) {
-		if ($myiface['vmps'] && $myiface['vlan'] > 1) {
-			$query = " INSERT INTO port(switch,name,default_vlan,location) VALUES (";
-			$query .= "'$switch','".$myiface['name']."','".$myiface['vlan']."','$location');";
-			echo "$idx - $query\n";
-		//        mysql_query($query) or die("unable to query");
-			unset($query);
-		};
+$flagscannow=0;
+for ($i=0;$i<$argc;$i++) {
+// TODO switch instead of if
+   if ($argv[$i]=="--switch") {
+	  $single = TRUE;
+	  $singleswitch = $argv[$i+1];
 	};
-	unset($ifaces);
-	exit();
 };
 
-exit();
 
+if (!$single) {
+	$switches =  mysql_fetch_all("SELECT * FROM switch");
+} else {
+	$switches =  mysql_fetch_all("SELECT * FROM switch WHERE name='$singleswitch'");
+};
+
+if (is_array($switches)) {
+
+	foreach ($switches as $switchrow) {
+		$switch = $switchrow['ip'];
+		$switchname = $switchrow['name'];
+		$location = $switchrow['location'];
+	        debug2("snmpwalk $switch for interfaces");
+
+		$ifaces = walk_ports($switch,$snmp_ro);
+  	    if (count($ifaces) > 0) {
+		 foreach ($ifaces as $idx => $myiface) {
+			if ($myiface['vmps'] && $myiface['vlan'] > 0) {
+				if (iface_exist($switch,$myiface['name'])) {
+					$query = "UPDATE port SET default_vlan='".$myiface['vlan']."' WHERE ";
+					$query .= "switch='$switch' AND name='".$myiface['name']."';";
+				} else {
+					$query = "INSERT INTO port(switch,name,default_vlan,location) VALUES (";
+					$query .= "'$switch','".$myiface['name']."','".$myiface['vlan']."','$location');";
+
+				};
+				echo "$query\n";
+			//        mysql_query($query) or die("unable to query");
+				unset($query);
+			};
+		 };
+        };
+		unset($ifaces);
+	};
+};
 
   // measure performance
    $mtime = microtime();
