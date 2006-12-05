6135bf6fc3b8f554e125982de99323f03a50578e
Sean Boran <sean@boran.com>
2006-12-05 09:12:46 +0000

    nmp_import.php snmp_defs.inc: Add lots of debugs, comments, support for CatOS

--- contrib/snmp_import.php
+++ contrib/snmp_import.php
@@ -1,14 +1,21 @@
-#!/usr/bin/php -f
+#!/usr/bin/php -- -f
 <?php
 /**
  * /opt/nac/contrib/snmp_import.php
  *
  * Long description for file:
- * - Get all current active computers and insert them into the systems table
- * - Get all switch port and current vlan and insert them into the ports table
+ * The snmp_import.php script is a script that is meant to be used during the deployment phase
+ * - get the actual configuration of non-trunk ports of the switches and 
+ *   populate the port table.  
+ * - ignore ports with vlan=0, and take the current vlan so that it can be used as a port 
+ *   default vlan
+ * - the output is SWL that you should review debore executing
+ * see also README:snmp_import, snmp_defs.inc, config.inc
+ * Enable $debug_flag1 and $debug_flag2 the first time you use this.
  *
  * On IOS do "show ip arp" - "show vlan"
  *        or "sh ip arp vrf insec"
+ * On CatOS so "show port status"
  * Further reading: 
  *    http://www.cisco.com/public/sw-center/netmgmt/cmtk/mibs.shtml
  *    The "getif" tool for exploring MIBs.
@@ -28,16 +35,24 @@
  *
  */
 
-$debug_flag1=false;
-$debug_flag2=false;
 
-include_once "../bin/funcs.inc";               # Load settings & common functions
-include_once "snmp_defs.inc";
-$debug_to_syslog=TRUE;
+# Php weirdness: change to script dir, then look for includes
+chdir(dirname(__FILE__));
+set_include_path("../:./");
+require_once "bin/funcs.inc";               # Load settings & common functions
+require_once "snmp_defs.inc";
 
 define_syslog_variables();              # not used yet, but anyway..
 openlog("import_snmp", LOG_PID, LOG_LOCAL5);
 
+// Enable debugging to understand how the script works
+  $debug_flag1=false;
+  $debug_flag2=false;
+  $debug_flag1=true;
+  $debug_flag2=true;
+  $debug_to_syslog=FALSE;
+
+debug2("Checking for SNMP: " . SNMP_OID_OUTPUT_FULL);
 snmp_set_oid_numeric_print(TRUE);
 snmp_set_quick_print(TRUE);
 snmp_set_enum_print(TRUE); 
@@ -50,23 +65,26 @@ snmp_set_enum_print(TRUE);
 
 db_connect();
 
-
-
-$flagscannow=0;
+// command line:
+//   Look for: snmp_import.php -switch sw0503
+//   otherwise scan all switches
+// TODO switch instead of if, make sure there are 3 args!
+$single = FALSE;
 for ($i=0;$i<$argc;$i++) {
-// TODO switch instead of if
-   if ($argv[$i]=="--switch") {
+   if ($argv[$i]=='-switch') {   // even if user gives --switch we see -switch
 	  $single = TRUE;
 	  $singleswitch = mysql_real_escape_string($argv[$i+1]);
 	};
 };
 
-
 if (!$single) {
-	$switches =  mysql_fetch_all("SELECT * FROM switch");
+        $switches =  mysql_fetch_all("SELECT * FROM switch");
+	debug1("Scanning all switches in the Database");
 } else {
-	$switches =  mysql_fetch_all("SELECT * FROM switch WHERE name='$singleswitch'");
+        $switches =  mysql_fetch_all("SELECT * FROM switch WHERE name='$singleswitch'");
+	debug1("Scanning one switch: $singleswitch");
 };
+
 
 if (is_array($switches)) {
 
@@ -74,12 +92,13 @@ if (is_array($switches)) {
 		$switch = $switchrow['ip'];
 		$switchname = $switchrow['name'];
 		$location = $switchrow['location'];
-	        debug2("snmpwalk $switch for interfaces");
+        	debug2("snmpwalk $switch,$switchname,$location for interfaces");
 
 		$ifaces = walk_ports($switch,$snmp_ro);
   	    if (count($ifaces) > 0) {
 		 foreach ($ifaces as $idx => $myiface) {
 			if ($myiface['vmps'] && $myiface['vlan'] > 0) {
+                		debug2("Vmps candidates vlan>0: $switchname interfaces " .$myiface['name'] .', vlan=' .$myiface['vlan'] );
 				if (iface_exist($switch,$myiface['name'])) {
 					$query = "UPDATE port SET default_vlan='".$myiface['vlan']."' WHERE ";
 					$query .= "switch='$switch' AND name='".$myiface['name']."';";
