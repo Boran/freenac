4b653c8289a9283effcbf94bf2c7de36e69bc545
Hector Ortiz <hector.ortiz@swisscom.com>
2007-09-24 14:04:01 +0000

    Class required for vmps_lastseen. Rename later

---
+++ lib/Result.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Logger.php
+ *
+ * Long description for file:
+ *
+ * This file defines the template Request class. This class doesn't define anything, 
+ * it is here only for completeness' sake
+ *
+ * PHP version 5
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * @package                     FreeNAC
+ * @author                      Hector Ortiz (FreeNAC Core Team)
+ * @copyright                   2007 FreeNAC
+ * @license                     http://www.gnu.org/copyleft/gpl.html   GNU Public License Version 2
+ * @version                     SVN: $Id$
+*/
+
+abstract class Result {
+   protected $port;
+   protected $system;
+}
+
+?>
