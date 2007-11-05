c21cec742dc5eb8f76291ae78998d062e48b0c80
Sean Boran <sean@boran.com>
2007-11-05 20:21:36 +0000

    SB: add explanatory text when an alert is generated (monitor2.sh)

--- bin/monitor2.sh
+++ bin/monitor2.sh
@@ -16,8 +16,6 @@ subject="FreeNAC warning: switch communication"
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/sbin:/usr/local/bin
 tempfile2=/tmp/monitor2.$$
 
-messagecount=10;
-
 /opt/nac/bin/logtail /var/log/messages /var/log/.messages.logtail1 | egrep "MACNOT|00000000" > $tempfile2 2>&1
 
 # Alert by email and senting to log in DB
@@ -26,6 +24,10 @@ if [ -s $tempfile2 ] ; then
   # Log events to vmpslog table, so GUI can see it.
   #echo "warning: switch communication" | /opt/nac/bin/vmps_log
 
+  echo " " >> $tempfile2
+  echo " " >> $tempfile2
+  echo "This means that a switch was not able to contact a VMPS server for a while (it probably had to query an alternative server), the 00000 message happens when contact is reestablished. It does not affect end-users, but might be an indication of communications problems between the Switches and VMPS serversi (or that the vmpsd_external daemon was daed), especially if it happens hourly/daily. " >> $tempfile2
+  echo " " >> $tempfile2
   echo " " >> $tempfile2
   echo "This email was generated from the root cron on `uname -n` by $0" >> $tempfile2
 
