309965e9494e16790c60035fe487b2800435e8ec
Hector Ortiz <hector.ortiz@swisscom.com>
2007-11-05 09:38:11 +0000

    Changed text in check_calling_function method

--- lib/EndDevice.php
+++ lib/EndDevice.php
@@ -359,9 +359,9 @@ EOF;
       $backtrace = debug_backtrace();
       array_shift($backtrace);	//Remove call to check_calling_method from the backtrace;
       $ok=0;
-      $valid_methods=array('update','insertIfUnknown');
+      #$valid_methods=array('update','insertIfUnknown');
       #Check if we are calling from a valid method
-      if (in_array($backtrace[0]['function'],$valid_methods))
+      #if (in_array($backtrace[0]['function'],$valid_methods))
       {
          #Check if we are using callwrapper
          if ( (strcasecmp($backtrace[2]['class'],'Callwrapper')==0) && (strcasecmp($backtrace[2]['function'],'__call')==0))
@@ -369,7 +369,7 @@ EOF;
             #Check if the class is a child of Policy and if calling method is postconnect
             if ( ($backtrace[4]['class'] instanceof Policy) && (strcasecmp($backtrace[4]['function'],'postconnect')!=0) )
             {
-               $this->logger->logit("{$backtrace[0]['function']} method can only be called from a postconnect method but called instead from {$backtrace[4]['function']}, condition not met, aborting insert operation",LOG_WARNING);
+               $this->logger->logit("{$backtrace[0]['function']} method must only be called from a postconnect, since it changes the database. Pre-connnect can run secondary servers (which have read-only tables), and must not change the DB. This method was called from {$backtrace[4]['function']}. Aborting insert operation",LOG_WARNING);
                return false;
             }
             else
