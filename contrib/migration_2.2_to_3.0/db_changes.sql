b8316376c5d7d61baf790b9916b9ec478589e9b2
Hector Ortiz <hector.ortiz@swisscom.com>
2007-11-12 14:29:13 +0000

    More changes to DB. group_id to systems, vlan_id to switches (contrib/opennac_db.tar.gz, contrib/migration_2.2_to_3.0/db_changes.sql)

--- contrib/migration_2.2_to_3.0/db_changes.sql
+++ contrib/migration_2.2_to_3.0/db_changes.sql
@@ -3,7 +3,7 @@ CREATE TABLE `health` (
 	`id` int(10) unsigned not null,
 	`value` varchar(30) not null,
 	`color` varchar(6) default null,
-	`comment` varchar(100) not null,
+	`comment` varchar(100) default null,
 	PRIMARY KEY (`id`);
 );
 INSERT INTO health(id, value, comment) VALUES (0,'Unknown','No health information available');
@@ -48,3 +48,35 @@ INSERT INTO config SET type='string', name='wsus_db';
 INSERT INTO config SET type='boolean', name='epo_enabled', value='false';
 INSERT INTO config SET type='string', name='epo_dbalias';
 INSERT INTO config SET type='string', name='epo_db';
+INSERT INTO config SET type='boolean', name='restart_daemons', value='false';
+
+-- DB fixes
+alter table systems change column description description varchar(100) default null comment "v3: not used";
+alter table systems change column uid uid int(11) default null;
+alter table systems change column ChangeDate ChangeDate varchar(100) default null;
+alter table systems change column ChangeUser ChangeUser int(11) default null;
+alter table systems change column LastPort LastPort int(11) default null;
+alter table systems change column LastVlan LastVlan int(11) default null;
+alter table systems change column os os int(11) default null;
+alter table systems change column os1 os1 int(10) default null;
+alter table systems change column os2 os2 int(10) default null;
+alter table systems change column os3 os3 int(10) default null;
+alter table systems change column os4 os4 varchar(64) default null;
+alter table systems change column class class int(11) unsigned default null;
+alter table systems change column class2 class2 int(11) unsigned default null;
+alter table switch change column ip ip varchar(20) default null;
+alter table switch change column location location int(11) default null;
+alter table switch change column `comment` `comment` varchar(50) default null;
+alter table port change column default_vlan default_vlan int(11) default null;
+alter table port change column last_vlan last_vlan int(11) default null;
+alter table port change column auth_profile auth_profile int(11) unsigned default null;
+alter table vlanswitch change column vlan_id vlan_id int(11) default null;
+alter table guirights change column ad_group ad_group varchar(255) default null;
+alter table patchcable change column rack_location rack_location varchar(30) default null;
+alter table patchcable change column outlet outlet varchar(30) default null;
+alter table patchcable change column other other varchar(30) default null;
+alter table patchcable change column `comment` `comment` varchar(30) default null;
+alter table services change column description description varchar(255) default null;
+alter table subnets change column scan scan tinyint(4) default '0';
+alter table switch add column vlan_id int default null;
+alter table systems add column group_id int default null;
