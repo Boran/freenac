80f54c8a92d68596368392ea059617bc218ac87f
Thomas Dagonnier <thomas.dagonnier@gmail.com>
2007-12-20 08:32:55 +0000

    Tail a given logfile according to configuraiton file (logtail_file, logtail_length)

---
+++ web/logtail.php
@@ -0,0 +1,51 @@
+<?php
+
+dir(dirname(__FILE__));
+set_include_path("./:../");
+
+// include configuration
+require_once('../etc/config.inc');
+// include functions
+require_once('webfuncs.inc');
+
+function logtail($filename,$length)
+{
+	if (!$length) { $length=10; };
+	$logfile = fopen($filename,'r');
+        exec("/usr/bin/tail -n $length $filename", $logtext, $error);
+
+	$text .= "<h1>Logfile : $filename</h1>";
+	if ($error){
+	    $text .= "Tail Error: $error\n";
+	} else {
+	    $text .= "\nLast $length lines :\n<p>\n<pre>\n";
+	    foreach ($logtext as $logline) {
+		$text .= $logline."\n";
+	    };
+	};
+	return($text);
+}
+
+if ($ad_auth===true)
+{
+   $rights=user_rights($_SERVER['AUTHENTICATE_USERPRINCIPALNAME']);
+   if ($rights>=1)
+   {
+      echo header_read();
+      echo main_stuff();
+      echo main_menu();
+      echo read_footer();   
+   }
+   else echo "<h1>ACCESS DENIED</h1>";
+}
+else
+{
+	
+   echo header_read();
+   echo main_stuff();
+   echo logtail($logtail_file,$logtail_length);
+   echo read_footer();
+}
+
+
+?>
