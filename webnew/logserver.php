1cd672f2ef220c562bba11681b8a485fab6b24f3
Sean Boran <sean@boran.com>
2008-03-03 14:00:09 +0000

    webnew: hide action column if not needed

--- webnew/logserver.php
+++ webnew/logserver.php
@@ -43,14 +43,14 @@ if ($_SESSION['nac_rights']<1) {
   throw new InsufficientRightsException($_SESSION['nac_rights']);
 } 
 else if ($_SESSION['nac_rights']==1) {
-  $action_menu=array('');   // no options
+  $action_menu='';
 }
 else if ($_SESSION['nac_rights']==2) {
-  $action_menu=array('');   // no options
+  $action_menu='';
   //$action_menu=array('Print','Edit');   // 'buttons' in action column
 }
 else if ($_SESSION['nac_rights']==99) {
-  $action_menu=array('');   // no options
+  $action_menu='';
   //$action_menu=array('Print', 'Edit', 'Delete');   // 'buttons' in action column
 }
 
