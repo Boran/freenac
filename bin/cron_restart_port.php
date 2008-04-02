88fabb0bc8353c5510d2dc5132c988b95961115b
Hector Ortiz <hector.ortiz@swisscom.com>
2008-04-02 11:36:41 +0000

    Wrong function call (bin/cron_restart_port)

--- bin/cron_restart_port.php
+++ bin/cron_restart_port.php
@@ -186,7 +186,7 @@ foreach ($switch_ports as $switch => $properties)
       else if ($properties['auth_profile'][$i] == '2')
       {
          #Check if the port is static. If it is, program it, otherwise, don't do anything (CatOS issues)?
-         if (array_find_key($port_index, $vm_type, '.', 1) == '1')
+         if (array_find_value($port_index, $vm_type, '.', 1) == '1')
          {
             set_port_as_dynamic($switch, $port, $port_index);
             $dont_restart++;
