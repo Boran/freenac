d36e2490ee8b35969f60ffdda79a039e670f082e
Sean Boran <sean@boran.com>
2008-05-09 18:29:10 +0000

    web: start on search page search.php ../lib/GuiSearch.php"

---
+++ lib/GuiSearch.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ *
+ *
+ * @package     FreeNAC
+ * @author      Many: S.Boran, T.Dagonnier, P.Bizeau
+ * @copyright   2008 FreeNAC
+ * @license     http://www.gnu.org/copyleft/gpl.html   GNU Public License Version 3
+ * @version     SVN: $Id$
+ * @link        http://freenac.net
+ *
+ */
+
+
+class GuiSearch extends WebCommon
+{
+  private $id, $action;      // See also WebCommon and Common
+
+
+  function __construct($action, $debug_level=1)
+  {
+    parent::__construct(false);     // See also WebCommon and Common
+    $this->logger->setDebugLevel($debug_level);
+    $this->debug("GuiSearch__construct id=$id, debug=$debug_level, action=$action", 2);
+
+    // verify/clean 'action'
+    // Now, have we a REQUEST action to carry out?
+    if ( !isset($action) ) {
+       throw new InvalidWebInputException("No action ");
+    }
+    $this->action=validate_webinput($action);
+
+  }
+
+  protected function print_title($title)
+  {
+    echo $this->print_header();
+    echo "<div id='GuiList1Title'>{$title}</div>";
+    //$this->debug($_SESSION['login_data'] .":Id=$id:" , 1);
+  }
+
+  public function handle_request()
+  {
+    $action=$this->action;
+    #global $_SESSION, $_REQUEST;
+    #$_REQUEST=array_map('validate_webinput',$_REQUEST);
+    $this->debug("handle_request() $action", 2);
+
+    if (isset($action)) {
+      if ($action==='Search') {
+        $this->print_title('Search for end devices');
+        echo $this->SearchForm();
+        #echo $this->query();
+        echo $this->print_footer();
+
+      } else {
+        // do nothing, action does not concern us.
+      }
+    }
+  }
+
+  public function SearchForm()
+  {
+    $this->debug("SearchForm()", 3);
+    #var_dump($_REQUEST);
+    if ($_SESSION['nac_rights']<1)
+      throw new InsufficientRightsException('SearchForm() ' .$_SESSION['nac_rights']);
+    $conn=$this->getConnection();     //  make sure we have a DB connection
+
+    // Clean inputs from the web, (security). Use _REQUEST to
+    // allow both GET (automation) or POST (interactive GUIs)
+    $_REQUEST=array_map('validate_webinput',$_REQUEST);
+    $cur_mac=$this->htmlescape($_REQUEST['searchstring']);
+    $cur_mac= empty($cur_mac) ? '0006.5bdd.a929' : $cur_mac;
+
+    $output=<<<TXT
+<form action="search.php" method="GET">
+<tr>
+  <td width="87">MAC Address:</td> 
+  <td width="400"> <input name="searchstring" type="text" value="{$cur_mac}"/></td>
+</tr>
+<tr>
+  <td><input type="submit" name="action" class="bluebox" value="Search" 
+   title='Find devices with this mac, which can be in 3 formats XXX.XXX.XXX XXXXXXXXXXXX or XX:XX:XX:XX:XX:XX'/> </td>
+</tr>
+</form>
+TXT;
+
+
+    return $output;
+  }
+  
+
+} // class
+
+
+
+/////////// main() should never get here .. ///////////////////////////////////////
+
+if (isset($_POST['action']) && $_POST['action']=='Edit') {
+  $logger=Logger::getInstance();
+  $logger->debug("GuiSearch:action:". $_POST['action'], 1);
+}
+
+if ( isset($_POST['submit']) ) {             // form submit, check fields
+## Initialise (standard header for all modules)
+  dir(dirname(__FILE__)); set_include_path("./:../");
+  require_once('webfuncs.inc');
+  $logger=Logger::getInstance();
+  $logger->setDebugLevel(1);
+  $logger->debug("GuiSearch main -submit");
+  #echo handle_submit();
+
+} else {
+  # Do nothing, we've been included.
+}
+
+?>
+
---
+++ web/search.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ *
+ * search.php
+ *
+ * Long description for file:
+ * List End-devices, sort, call the editing screen
+ *
+ * @package     FreeNAC
+ * @author      FreeNAC Core Team
+ * @copyright   2008 FreeNAC
+ * @license     http://www.gnu.org/copyleft/gpl.html   GNU Public License Version 3
+ * @version     SVN: $Id$
+ * @link        http://freenac.net
+ *
+ */
+
+## Initialise (standard header for all modules)
+  dir(dirname(__FILE__)); set_include_path("./:../lib:../");
+  require_once('webfuncs.inc');
+  $logger=Logger::getInstance();
+  $logger->setDebugLevel(1);
+
+  ## Loggin in? User identified?
+  include 'session.inc.php';
+  check_login(); // logged in?
+  #$logger->debug('Start, uid=' .$_SESSION['uid'], 3);
+## end of standardc header ------
+
+
+### --------- main() -------------
+
+
+$_SESSION['caller']=basename($_SERVER['SCRIPT_FILENAME']);
+// set parameters  
+$title="List of End-devices ";
+$sortlimit=200;
+$sortby='sys.LastSeen';
+$searchby='sys.mac';
+
+$_REQUEST=array_map('validate_webinput',$_REQUEST);
+$searchstring=$_REQUEST['searchstring'];
+$searchstring=normalise_mac($searchstring);
+
+// 1. Check rights
+if ($_SESSION['nac_rights']<1) {
+  throw new InsufficientRightsException($_SESSION['nac_rights']);
+}
+else if ($_SESSION['nac_rights']==1) {
+  $action_menu=array('View');   // no options
+  $action_confirm=array('');     // no confirmation popups
+}
+else if ($_SESSION['nac_rights']==2) {
+  $action_menu=array('View','Edit', 'Restart Port');   // 'buttons' in action column
+  $action_confirm=array('', '');        // no confirmation popups
+}
+else if ($_SESSION['nac_rights']==4) {
+  $action_menu=array('View', 'Restart Port');   // 'buttons' in action column
+  $action_confirm=array('', '');        // no confirmation popups
+}
+else if ($_SESSION['nac_rights']==99) {
+  $action_menu=array('View', 'Edit', 'Restart Port', 'Delete');   // 'buttons' in action column
+  $action_confirm=array('', '', '', 'Really DELETE the record of this End-Device?');  // Confirm Deletes
+} else {
+  throw new InsufficientRightsException("Unknown nac_rights: ".$_SESSION['nac_rights']);
+}
+
+
+## A smaller and quicker query:
+$action_fieldname="Index";     $idx_fieldname="sys.id";
+$q=<<<TXT
+SELECT 
+  sys.name as Systemname, 
+  sys.mac as 'MAC Address', 
+  status.value as Status, 
+
+  vlan.default_name as Vlan, lvlan.default_name as LastVlan, 
+
+  sys.lastseen AS 'LastSeen Layer2',
+  sys.r_ip AS 'LastSeen Layer3 IP Addr.',
+  sys.r_timestamp 'Last IP: time',
+  sys.last_hostname AS 'Last IP: DNS name',
+
+  sys.comment, 
+  b.name as building, loc.name as office, 
+  swi.name as Switch,
+  p.name as port, pcloc.name as PortLocation, p.comment as PortComment,
+  swloc.name as SwitchLocation,
+  usr.username as Username, 
+  usr.surname AS Firstname, usr.givenname AS FamilyName, usr.department, 
+  usr.telephonenumber as UserTelephone,
+  sys.os4 as OS4,
+  $idx_fieldname AS '$action_fieldname'
+    FROM systems as sys LEFT JOIN vlan as vlan ON vlan.id=sys.vlan
+      LEFT JOIN vlan as lvlan ON lvlan.id=sys.lastvlan
+      LEFT JOIN vstatus as status ON status.id=status
+      LEFT JOIN users as usr ON usr.id=sys.uid
+      LEFT JOIN users as cusr ON cusr.id=sys.changeuser
+      LEFT JOIN location as loc ON loc.id=sys.office
+      LEFT JOIN building as b ON b.id=loc.building_id
+      LEFT JOIN port as p ON p.id=sys.lastport
+      LEFT JOIN patchcable as pc ON pc.port=p.id
+      LEFT JOIN location as pcloc ON pcloc.id=pc.office
+      LEFT JOIN switch as swi ON swi.id=p.switch
+      LEFT JOIN location as swloc ON swloc.id=swi.location
+      LEFT JOIN location as usrloc ON usrloc.id=usr.location
+      LEFT JOIN sys_os as sos ON sos.id=sys.os
+      LEFT JOIN sys_os1 as sos1 ON sos1.id=sys.os1
+      LEFT JOIN sys_os2 as sos2 ON sos2.id=sys.os2
+      LEFT JOIN sys_os3 as sos3 ON sos3.id=sys.os3
+      LEFT JOIN sys_class as sclass ON sclass.id=sys.class
+      LEFT JOIN sys_class2 as sclass2 ON sclass2.id=sys.class2
+TXT;
+
+# sys.inventory
+
+// Actions handled by GuiEditDevice class
+if (isset($_REQUEST['action']) && (
+       ($_REQUEST['action'] == 'Update')
+    || ($_REQUEST['action'] == 'Delete')
+    || ($_REQUEST['action'] == 'Edit')
+    || ($_REQUEST['action'] == 'Add')
+    || ($_REQUEST['action'] == 'Restart Port')
+
+  ) ) {
+
+  if (isset($_REQUEST['action_idx']) )
+    $action_idx=$_REQUEST['action_idx'];
+  else
+    $action_idx=0;
+
+  $logger->debug("listall > GuiEditDevice idx=$action_idx, action=". $_REQUEST['action'], 1);
+  $report=new GuiEditDevice($_REQUEST['action'], $action_idx, 2);  // last param=debug
+  $report->handle_request();
+  $logger->debug("after new GuiEditDevice", 3);
+
+} else if (isset($_REQUEST['action']) && ($_REQUEST['action'] == 'Search') ) {
+  require_once "GuiList1_control.php";
+
+} else {
+  // Default & Actions handled by the Search class: show the basic search form
+  $report=new GuiSearch('Search', 3);
+  $report->handle_request();
+  #$logger->debug("after new GuiSearch ", 3);
+}
+
+
+?>
