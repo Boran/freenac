b8f1d9cf0a1715b3371f1d084c54c0be36306b04
Sean Boran <sean@boran.com>
2008-06-30 14:42:40 +0000

    SB: Add comments, dns updates, test. generate_dnsupdate2.php

--- contrib/generate_dnsupdate2.php
+++ contrib/generate_dnsupdate2.php
@@ -1,8 +1,23 @@
+#!/usr/bin/php
 <?php
 /**
- * /opt/nac/contrib/generate_dns.php
+ * contrib/generate_dns2.php
  *
- * Generate Forward DNS configuration file from the FreeNAC database
+ * Long description for file:
+ * Using ip.address and systems.name from the FreeNAC 'ip' DB, generate 
+ * a list of dynamic DNS updates.
+ * The DNS update commands are written to $tmp_file, once the file has been 
+ * written, the dns_update flag is reset for each field.
+ * Inputs: ip.address, systems.name
+ *   $conf->: ddns_server,  dns_domain, ddns_ttl
+ * Options:  
+ *   $ddns_level: 0 = all hosts, 1 = hosts with dns_update bit 1 is set
+ *  Status field
+ *    -1 = special adress (network address, broadcast)
+ *    0 = free
+ *    1 = used, dynamic
+ *    2 = fixed
+ *    3 = reserved
  *
  * PHP version 5
  *
@@ -12,20 +27,38 @@
  *
  * @package			FreeNAC
  * @author			Thomas Dagonnier (FreeNAC Core Team)
- * @copyright			2007 FreeNAC
+ * @copyright			2008 FreeNAC
  * @license			http://www.gnu.org/copyleft/gpl.html   GNU Public License Version 2
  * @version			SVN: $Id$
  * @link			http://www.freenac.net
  *
  */
-include_once('/opt/nac/web/webfuncs.inc');
-include_once('/opt/nac/etc/config.inc');
 
+// settings
+$nsupdate="nsupdate -d ";    // -d = debug
+$ddns_level=0; /* DDNS level: 0 = all hosts, 1 = hosts with dns_update bit 1 is set */
+
+
+//  main ()
+chdir(dirname(__FILE__));
+set_include_path("./:../:../bin");
+require_once("../bin/funcs.inc.php");
+#include_once('/opt/nac/web/webfuncs.inc');
+#include_once('/opt/nac/etc/config.inc');
+
+$logger=Logger::getInstance();
+$logger->setDebugLevel(3);
+$logger->setLogToStdErr(false);
+
+
+$logger->debug("ddns import started", 1);
 db_connect($dbuser,$dbpass);
 
 #$tmp_dir = '/tmp';
 #$tmp_file = 'nsupdate'.date("ymdHi");
 $tmp_file = system('mktemp');
+#$outfile = $tmp_dir.'/'.$tmp_file;
+$outfile = $tmp_file;
 
 /*** A Records & aliases (CNAME)  ************************************************/
 
@@ -38,11 +71,6 @@ function sanitize_name($name) {
         return($name);
 };
 
-/* DDNS level :
-        0 = all hosts
-        1 = hosts with dns_update bit 1 is set
-*/
-$ddns_level=0;
 switch($ddns_level) {
 		case 1 :
 			$query = "SELECT ip.id as id, INET_NTOA(ip.address) as ip, systems.name as name, ip.dns_update as dns_update FROM ip LEFT JOIN systems ON ip.system = systems.id WHERE ip.system != 0 AND ((ip.dns_update & 1) = 1)";
@@ -53,6 +81,7 @@ switch($ddns_level) {
 };
         $res = mysql_query($query) or die("Unable to query MySQL : $query; \n");
 
+        $dns_ina='';
         if (mysql_num_rows($res) > 0) {
                 while ($host = mysql_fetch_array($res)) {
 			if (($host['name'] != 'unknown') && ($host['ip'] != '') && ($host['name'] != '')) { 
@@ -78,17 +107,21 @@ switch($ddns_level) {
 	SVR
 */
 
-$dns_update = "server $ddns_server\r\n";
+$dns_update = "server $conf->ddns_server\r\n";
 //$dns_update .= "zone $dns_domain\r\n"; Zone must be in name
 $dns_update .= $dns_ina;
 $dns_update .= "send\n";
 
-		$outfile = $tmp_dir.'/'.$tmp_file;
-		$fp = fopen($outfile,'w');
-		fwrite($fp,$dns_update);
-		fclose($fp);
+  $fp = fopen($outfile,'w');
+  fwrite($fp,$dns_update);
+  fclose($fp);
+  $logger->debug("dns_update=" .$dns_update, 3);
 
-// TODO : send file to dns server (actually execute the nsupdate)
-// TODO delete temporary file
+// send file to dns server (actually execute the nsupdate)
+  $res=syscall("$nsupdate $tmp_file");
+  $logger->debug("answer=" .$res, 1);
+
+// delete temporary file
+  unlink($tmp_file);
 
 ?>
